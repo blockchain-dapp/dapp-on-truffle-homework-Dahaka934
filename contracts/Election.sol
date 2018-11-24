501e120baed4165ff4765152a962b2ad608d2a91
Dahaka934 <dahaka934@gmail.com>
2018-11-24 18:37:33 +0300

    Add multi candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,23 @@
 pragma solidity 0.4.24;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    // Constructor
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
